72c4fa69edd67be1312bcb74849bf721394e6648
Passant Ibrahim <70277570+passantibrahimem@users.noreply.github.com>
2023-01-05 16:10:29 +0200

    Add files via upload
    that map the classes to tables in db so you can track any change in classes from migrations files

---
+++ Migrations/20230104235905_InsertEmployee.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DBMigrationV1.Migrations
+{
+    public partial class InsertEmployee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"Insert into Employee
+(Id,Name,Email,MobilePhone,AnnualBalance,SickBalance)
+Values
+(1,'Passant Ibrahim','passantibrahim96@gmail.com','01159455077',21,6)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"Delete from Employee where id=1");
+        }
+    }
+}
