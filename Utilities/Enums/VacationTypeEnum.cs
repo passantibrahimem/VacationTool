0dea933ffbb2ef4da0223a6dc0af403dc64e3450
Passant Ibrahim <70277570+passantibrahimem@users.noreply.github.com>
2023-01-05 15:58:07 +0200

    Add files via upload

---
+++ Utilities/Enums/VacationTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Enums
+{
+    public enum VacationTypeEnum
+    {
+        Annual=1,
+            Sick=2
+    }
+}
